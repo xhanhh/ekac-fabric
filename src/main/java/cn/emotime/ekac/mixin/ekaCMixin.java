b723e01f73c08062a2c65313a03b48f4e113d745
emotime <18201087577@163.com>
2022-05-15 13:30:59 +0800

    revise mixin

--- src/main/java/cn/emotime/ekac/mixin/ekaCMixin.java
+++ src/main/java/cn/emotime/ekac/mixin/ekaCMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ekaCMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ekaC.LOGGER.info("This line is printed by an example mod mixin!");
+		ekaC.LOGGER.info("Wow, it's amazing!");
 	}
 }
