b10623360425dd2c7f6b5df6f4cd07b81d595562
emotime <18201087577@163.com>
2022-12-31 11:26:55 +0800

    -再次移除冗余日志输出

--- src/main/java/top/ilov/mcmods/ekac/blocks/Blocks.java
+++ src/main/java/top/ilov/mcmods/ekac/blocks/Blocks.java
@@ -1,5 +1,7 @@
 package top.ilov.mcmods.ekac.blocks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import top.ilov.mcmods.ekac.itemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -14,6 +16,8 @@ import net.minecraft.util.registry.Registry;
 import top.ilov.mcmods.ekac.ekaC;
 
 public class Blocks {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(ekaC.MOD_ID);
 
     public static final CakeBlock EKAC = registerBlock("ekac", new ekaCBlock(FabricBlockSettings.of(Material.CAKE).hardness(1.5f)), itemGroup.TAB);
 
@@ -31,7 +35,7 @@ public class Blocks {
     }
 
     public static void registerModBlocks() {
-        ekaC.LOGGER.debug("Registering ekac block.");
+        LOGGER.debug("Registering ekac block.");
     }
 
 }
